328c8e98583a7dd8b1c53492af3c6426d95dfb31
藤原琢巳 <fujiwaratakumi@fujiwaratakuminoMacBook-Pro.local>
2022-01-21 16:41:07 +0900

    投稿ボタン作成まで

---
+++ demoapp2/components/SubmitForm.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <label></label>
+    <input type="text">
+    <button type="submit" class="btn btn-primary">投稿</button>
+  </div>
+</template>
+
--- demoapp2/pages/index.vue
+++ demoapp2/pages/index.vue
@@ -1,9 +1,15 @@
 <template>
-  <Tutorial/>
+  <div>
+    <SubmitForm/>
+  </div>
 </template>
 
 <script>
+import SubmitForm from '~/components/SubmitForm.vue';
 export default {
+  components: {
+    SubmitForm
+  },
   name: 'IndexPage'
 }
 </script>
