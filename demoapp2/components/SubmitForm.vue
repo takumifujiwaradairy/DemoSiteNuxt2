c6d4a367d229b5d1bebb31b348558c0fbb419b79
藤原琢巳 <fujiwaratakumi@fujiwaratakuminoMacBook-Pro.local>
2022-01-22 15:45:35 +0900

    送信機能

--- demoapp2/components/SubmitForm.vue
+++ demoapp2/components/SubmitForm.vue
@@ -1,8 +1,25 @@
 <template>
   <div>
     <label></label>
-    <input type="text">
-    <button type="submit" class="btn btn-primary">投稿</button>
+    <input type="text" v-model="title">
+    <p>{{title}}</p>
+    <button class="btn btn-primary" @click="submit()">投稿</button>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+      content: '',
+    }
+  },
+  methods: {
+    submit(title) {
+      console.log(this.title);
+      this.$store.commit('submit', {title: this.title, content: this.content});
+    }
+  }
+}
+</script>
