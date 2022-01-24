6756da086968145050cb404ea454ba28e5b8c45e
藤原琢巳 <fujiwaratakumi@fujiwaratakuminoMacBook-Pro.local>
2022-01-24 11:19:23 +0900

    検索機能仮

--- demoapp3/components/AddIndex.vue
+++ demoapp3/components/AddIndex.vue
@@ -1,11 +1,13 @@
 <template>
   <div class = "container">
     <ul>
-      <li v-for = 'article in aricles' :key = 'article.id'>
+      <li v-for = 'article in displayArticle' :key = 'article.id'>
         <p> {{ article.title }} </p>
         <p> {{ article.content }} </p>
       </li>
     </ul>
+    <input type="text" v-model="findTitle">
+    <button>検索</button>
   </div>
 </template>
 
@@ -13,8 +15,34 @@
 import {mapState} from 'vuex';
 
 export default {
+  data: function() {
+    return {
+      findTitle: '',
+    }
+  },
   computed: {
-    ...mapState(['aricles'])
+    ...mapState(['articles']),
+    // displayArticle: function() {
+    //   // let arr = [];
+    //   // let data = this.aricles;
+    //   // // data.forEach(element => {
+    //   // //   if(element.title.toLowerCase() == this.title.toLowerCase()){
+    //   // //     arr.push(element);
+    //   // //   }
+    //   // });
+    //   return arr;
+    // }
+    displayArticle: function() {
+      if(this.findTitle == 0){
+        return this.articles;
+      } else if(this.findTitle == articles.title){
+        return this.articles;
+      } else {
+        return this.findTitle;
+      }
+    }
+  },
+  methods: {
   }
 }
 </script>
--- demoapp3/store/index.js
+++ demoapp3/store/index.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex';
 const createStore = () => {
   return new Vuex.Store({
     state: () => ({
-      aricles: [
+      articles: [
         {title: 'test', content: 'hogehogehoge'}
       ]
     }),
