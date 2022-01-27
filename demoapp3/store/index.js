d1d4ffb208f48201d7cc878a2ba270322d3bd08e
藤原琢巳 <fujiwaratakumi@fujiwaratakuminoMacBook-Pro.local>
2022-01-27 22:17:04 +0900

    Delete未実装

--- demoapp3/store/index.js
+++ demoapp3/store/index.js
@@ -17,17 +17,46 @@ const createStore = () => {
     },
     actions: {
       async fetchArticles({commit}) {
-        await axios.get(url).then((responce => {commit('setArticles', responce.data)}))
+        await axios.get(url).then(responce => {
+          commit('setArticles', responce.data.data)
+        })
       },
       async postArticle({commit}, article) {
-        await axios.post(url, article).then(responce => {commit(responce.data)})
-      },
+      await axios.post(url, article).then(responce => {commit(responce.data)})
+      // async postArticle({commit}, article) {
+      //   const response = await axios.post(url, { article })
+      //   commit(response.data)
+      },  
       async deleteArticle({commit}, id){
-        await axios.delete(url.concat(`/${id}`)).then(() => {commit('id')})
+        await axios.delete(url.concat(`/${id}`)).then(() => {commit('deleteArticle', id)})
       }
     },
     mutations: {
-      setArticles: (state, articles) => (state.articles = articles),
+      setArticles: (state, articles) =>{
+        console.log(articles);
+        state.articles = articles
+      },
+      deleteArticle: (state, id) => {
+        state.articles.forEach((article) =>console.log(article.id))
+        state.articles.forEach((article) => {
+          {
+            console.log(article.id);
+            console.log(article);
+            console.log(id);
+            article.splice(article.id,1);
+          }
+        })
+        // if(state.articles.id === id){
+        //   console.log("AAA");
+        //   state.articles.splice(id,1);
+        //   return aaa;
+        // } else {
+        //   console.log("BBB");
+        //   console.log(state.articles.data[0].id);
+        //   console.log(id)
+        //   state.articles.splice(id,1);
+        // }
+      }
     }
   })
 }
