b22e8fa00286fd88894f42b2cc8e49da5850f080
藤原琢巳 <fujiwaratakumi@fujiwaratakuminoMacBook-Pro.local>
2022-01-31 17:04:55 +0900

    修正

--- backendapp3/config/routes.rb
+++ backendapp3/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     namespace :api do
       namespace :v1 do
         resources :articles
-        resources :likes ,only: [:update]
+        resources :likes, only: [:update]
       end
     end
   end
