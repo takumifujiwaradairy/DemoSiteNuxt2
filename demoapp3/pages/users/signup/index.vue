566b5581c0a998f5e66069e61c4ee0f10a3f94de
藤原琢巳 <fujiwaratakumi@fujiwaratakuminoMacBook-Pro.local>
2022-02-02 17:31:29 +0900

    登録画面作成まで

---
+++ demoapp3/pages/users/signup/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>新規登録</h1>
+    <div>
+      <form action="">
+        <label>メールアドレス</label>
+        <input type="email" v-model="email">
+        <label>パスワード</label>
+        <input type="password" v-model="password">
+        <label>パスワード確認</label>
+        <input type="password" v-model="passwordConfirmation">
+        <button @click="register">ログイン</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  auth: false,
+  data() {
+    return {
+      user: {
+        email: '',
+        password: '',
+        passwordConfirmation: ''
+      }
+    }
+  },
+  methods: {
+    register() {
+      this.$axios.post('/api/auth', this.user)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
