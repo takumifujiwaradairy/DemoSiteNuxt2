57432173df43f94733a7cc09f24000fe7168860b
藤原琢巳 <fujiwaratakumi@fujiwaratakuminoMacBook-Pro.local>
2022-02-02 16:16:57 +0900

    リンクの作成

--- demoapp3/layouts/TheHeader.vue
+++ demoapp3/layouts/TheHeader.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="container">
-    <h1>デモサイト</h1>
+    <h1>
+      <nuxt-link to="/">デモサイト</nuxt-link>
+    </h1>
+    <div>
+      <nuxt-link to="/users/login">ログイン</nuxt-link>
+      <nuxt-link to="/users/signup">新規登録</nuxt-link>
+      <nuxt-link to="/users/account">削除・ログアウト</nuxt-link>
+    </div>
   </div>
 </template>
