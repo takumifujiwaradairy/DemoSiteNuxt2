7f49f428bc096a6674ccc0f4930f4382ca2bb54c
藤原琢巳 <fujiwaratakumi@fujiwaratakuminoMacBook-Pro.local>
2022-02-02 14:36:31 +0900

    サーバーサイドのdevice設定

--- backendapp3/config/routes.rb
+++ backendapp3/config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
     namespace :api do
       namespace :v1 do
         resources :articles
         resources :likes ,only: [:update]
+        mount_devise_token_auth_for 'User', at: 'auth'
       end
     end
   end
